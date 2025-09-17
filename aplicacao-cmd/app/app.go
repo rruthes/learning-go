46e69653b82156c9c7be1be556f64412653a177b
Rafael Ruthes <rafaelruthes444@gmail.com>
2025-09-17 14:30:35 -0300

    aplicacao de linha de comando

---
+++ aplicacao-cmd/app/app.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func Gerar() *cli.App { // ponteiro de cli (pacote externo instalado) e app (tipo interno desse pacote)
+	app := cli.NewApp()
+	app.Name = "aplicação de linha de comando"
+	app.Usage = "busca IPs e nomes de servidor na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca ips de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+}
